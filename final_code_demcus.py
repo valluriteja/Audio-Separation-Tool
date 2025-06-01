1ef557bf03a1d1f1c8155616e3dd6c9efda5cf4d
Valluri-Teja <valluri.teja.in@gmail.com>
2025-06-01 07:56:05 +0530

    Add files via upload

---
+++ final_code_demcus.py
@@ -0,0 +1,9 @@
+import subprocess
+
+# Run Demucs on the new file "dia1.mp3"
+subprocess.run([
+    "C:\\Users\\TEJA\\AppData\\Local\\Packages\\PythonSoftwareFoundation.Python.3.11_qbz5n2kfra8p0\\LocalCache\\local-packages\\Python311\\Scripts\\demucs.exe", 
+    "D:\intern\Amitabh Bachchan.mp3"
+])
+
+print("Demucs finished processing!")
